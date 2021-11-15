7b4290fd5776e43f27d7f516d5b969ddf294d7cc
dhirendra1999sahoo <87979610+dhirendra1999sahoo@users.noreply.github.com>
2021-11-15 05:32:46 +0100

    Update StockExceptionHandler.java

--- src/main/java/com/dhirendra/StockExceptionHandler.java
+++ src/main/java/com/dhirendra/StockExceptionHandler.java
@@ -1,5 +1,63 @@
 package com.dhirendra;
 
-public class StockExceptionHandler {
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import com.dhirendra.exception.StockException;
+
+/**
+ * Exception handler class All Exceptions are handled in this class. All
+ * exceptions fall back here
+ * 
+ * @author
+ *
+ */
+public class StockExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+		logger.debug("handleHttpRequestMethodNotSupported :: There was an exception", ex);
+
+		final StringBuilder message = new StringBuilder();
+		message.append(ex.getMethod());
+		message.append(" HTTP Request method is not supported for this request. Supported methods are:");
+		ex.getSupportedHttpMethods().forEach(supportedMethod -> message.append(supportedMethod + " "));
+
+		return new ResponseEntity<Object>(new HttpHeaders(), HttpStatus.METHOD_NOT_ALLOWED);
+	}
+
+	/**
+	 * Handle All generic exception thrown by the application
+	 *
+	 * @param exception - Generic Exception thrown
+	 * @param request   - the WebRequest
+	 * @return {@link ResponseEntity} - the ResResponseEntity
+	 */
+	@ExceptionHandler({ Exception.class })
+	public ResponseEntity<Object> handleAllException(Exception exception) {
+		logger.warn("handleAllException :: There was an unknow exception", exception);
+		logger.debug("handleAllException :: There was an unknow exception");
+		return new ResponseEntity<Object>(new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	/**
+	 * Handle All StockException thrown by the application
+	 *
+	 * @param stockException - the application specific exception
+	 * @param request        - the WebRequest
+	 * @return {@link ResponseEntity} - the ResResponseEntity
+	 */
+	@ExceptionHandler({ StockException.class })
+	public ResponseEntity<?> handleRefException(final StockException stockException) {
+		logger.debug("handleRefException :: there was an StockException", stockException);
+
+		return new ResponseEntity<>(stockException.getMessage(), HttpStatus.NO_CONTENT);
+
+	}
 }
