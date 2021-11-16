e8ccdc19d2ab8d069e769cc29d4834c59572192c
dhirendra1999sahoo <87979610+dhirendra1999sahoo@users.noreply.github.com>
2021-11-16 13:24:19 +0100

    Update StockEntityToModelConverter.java

--- src/main/java/com/dhirendra/model/converter/StockEntityToModelConverter.java
+++ src/main/java/com/dhirendra/model/converter/StockEntityToModelConverter.java
@@ -4,13 +4,20 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 import com.dhirendra.entity.Stock;
+import com.dhirendra.model.StockDTO;
 
+/**
+ * This class converts Entity to Model
+ * 
+ * @author
+ *
+ */
 @Component
-public class StockEntityToModelConverter implements Converter<Stock, com.dhirendra.model.Stock> {
+public class StockEntityToModelConverter implements Converter<Stock, StockDTO> {
 
 	@Override
-	public com.dhirendra.model.Stock convert(Stock source) {
-		com.dhirendra.model.Stock target = new com.dhirendra.model.Stock();
+	public StockDTO convert(Stock source) {
+		StockDTO target = new StockDTO();
 		target.setName(source.getName());
 		target.setCurrentPrice(source.getCurrentPrice());
 		target.setLastUpdate(source.getLastUpdate());
