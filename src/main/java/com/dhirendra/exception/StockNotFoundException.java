0162298c4564e7773fe5775db312c653ea22fd26
dhirendra1999sahoo <87979610+dhirendra1999sahoo@users.noreply.github.com>
2021-11-21 22:14:13 +0100

    Update StockNotFoundException.java

--- src/main/java/com/dhirendra/exception/StockNotFoundException.java
+++ src/main/java/com/dhirendra/exception/StockNotFoundException.java
@@ -1,11 +1,17 @@
 package com.dhirendra.exception;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Application Sepcific Exception class
  * 
- * @author 
+ * @author
  *
  */
+
+@ResponseStatus(NOT_FOUND)
 public class StockNotFoundException extends RuntimeException {
 
 	/**
@@ -13,16 +19,8 @@ public class StockNotFoundException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 5673660358524295302L;
 
-	public StockNotFoundException(String message) {
-		super(message);
-	}
-
-	public StockNotFoundException(String errorMessage, Throwable err) {
-		super(errorMessage, err);
-	}
-
-	public StockNotFoundException(long id) {
-		
+	public StockNotFoundException(Long id) {
+		super("Could not find Stock with id " + id + ".");
 	}
 
 }
