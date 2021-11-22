bba906863664809cc939d171f6d5088d18f46394
dhirendra1999sahoo <87979610+dhirendra1999sahoo@users.noreply.github.com>
2021-11-22 18:47:48 +0100

    Update Stock.java

--- src/main/java/com/dhirendra/entity/Stock.java
+++ src/main/java/com/dhirendra/entity/Stock.java
@@ -22,7 +22,7 @@ public class Stock implements Serializable {
 	 */
 	private static final long serialVersionUID = 286850869828709792L;
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	@NotNull
 	@Size(min=2, max=10)
