5966a6e856092e9dee5286ed8fe1cd1729fd368d
dhirendra1999sahoo <87979610+dhirendra1999sahoo@users.noreply.github.com>
2021-11-23 09:53:06 +0100

    Update StockApplication.java

--- src/main/java/com/dhirendra/StockApplication.java
+++ src/main/java/com/dhirendra/StockApplication.java
@@ -3,7 +3,11 @@ package com.dhirendra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Stock API", version = "1.0", description = "Information about Stocks"))
 public class StockApplication {
 
 	public static void main(String[] args) {
